0fddca4af9f781695190b8ecfe7d32a40f2c762d
Eun Seok <jeseok95@gmail.com>
2020-12-20 07:20:40 +0900

    feat: 정적파일 관리하는 ResourceHttpHandler 추가

---
+++ src/main/java/webserver/servlet/ResourceHttpHandler.java
@@ -0,0 +1,31 @@
+package webserver.servlet;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
+
+import exception.NotFoundPathException;
+import exception.RequestHandleException;
+import http.HttpRequest;
+import http.response.HttpResponse;
+import http.response.HttpStatus;
+import utils.FileIoUtils;
+import webserver.StaticResource;
+
+public class ResourceHttpHandler {
+    public static void handleResource(HttpRequest httpRequest, HttpResponse httpResponse) {
+        try {
+            String requestPath = httpRequest.getPath();
+            String contentType = StaticResource.fromPath(requestPath).getContentType();
+            byte[] body = FileIoUtils.loadFileFromClasspath(
+                    StaticResource.STATIC_PATH + requestPath);
+
+            httpResponse.setStatusLine(httpRequest.getHttpVersion(), HttpStatus.OK);
+            httpResponse.setBody(body, contentType);
+        } catch (NoSuchElementException e) {
+            throw new NotFoundPathException("404 Not found");
+        } catch (IOException | URISyntaxException e) {
+            throw new RequestHandleException(e);
+        }
+    }
+}
